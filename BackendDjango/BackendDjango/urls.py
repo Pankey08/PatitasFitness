2ceddb7ca3adf271c966f7c33351a127a7205355
Carlos Madariaga <carlosmadadelrio1@gmail.com>
2022-05-07 12:13:31 -0600

    Creación Aplicación Login

--- BackendDjango/BackendDjango/urls.py
+++ BackendDjango/BackendDjango/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from BackendDjango.views import login
+from login.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
